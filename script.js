8d837d94df6d04e0e65565b97a5bf6feed017fd0
Rxanadu <ed.onuk31@gmail.com>
2022-08-18 10:52:51 -0400

    added functionality to add, remove books

--- script.js
+++ script.js
@@ -2,33 +2,121 @@ let myLibrary = [];
 
 var addModal = document.getElementById("add-modal");
 var closeModalBtn = document.querySelector(".close");
-var addBookBtn = document.getElementById("add-btn");
+var openModalBtn = document.querySelector("#open-modal");
+var addBookBtn = document.getElementById("add-book");
 
-function Book() {
+//reference form elements
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+
+var tempBook = new Book("test", "hello", 55);
+myLibrary.push(tempBook);
+console.log(myLibrary);
+console.log(tempBook.title);
+
+displayBooks();
+
+function Book(title, author, pages) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
 }
 
 function addBookToLibrary() {
   // do stuff here
-  newBook = new Book();
-  myLibrary.push(newBook);
+
+  if (
+    titleInput.value != "" &&
+    authorInput.value != "" &&
+    pagesInput.value != ""
+  ) {
+    newBook = new Book(titleInput, authorInput, pagesInput);    
+    myLibrary.push(newBook);
+
+    displayBooks();
+    closeAddBookForm();
+  } else {
+    alert("Please fill out all fields!");
+  }
 }
 
-function displayBooks(){
-    for(let book in myLibrary){
-        // display book
-        console.log(book);
-    }
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
 
-function openAddBookForm(){
-    addModal.style.display = "block";
+function displayBooks() {
+  let libraryList = document.querySelector(".library-list");
+
+  //clear list of books
+  removeAllChildNodes(libraryList);
+
+  for (let book in myLibrary) {
+    // display book
+    let libraryBook = document.createElement("li");
+    libraryBook.classList.add("book");
+
+    //create child elements for book
+    let bookTitle = document.createElement("p");
+    bookTitle.classList.add("book-title");
+    let bookAuthor = document.createElement("p");
+    bookAuthor.classList.add("book-author");
+    let bookPages = document.createElement("p");
+    bookPages.classList.add("book-pages");
+    let removeBookBtn = document.createElement("button");
+    removeBookBtn.classList.add("remove");
+    removeBookBtn.addEventListener("click", function(){
+        myLibrary.splice(book, 1);
+        displayBooks();
+        console.log(myLibrary.length);
+    });
+
+    //  add elements to book    
+    bookTitle.innerHTML = "Title: " + myLibrary[book].title;
+    bookAuthor.innerHTML = "Author: " + myLibrary[book].author;
+    bookPages.innerHTML = "Pages: " + myLibrary[book].pages;
+    removeBookBtn.innerHTML = "Remove";
+
+    libraryBook.appendChild(bookTitle);
+    libraryBook.appendChild(bookAuthor);
+    libraryBook.appendChild(bookPages);
+    libraryBook.appendChild(removeBookBtn);
+
+    libraryList.appendChild(libraryBook);
+  }
 }
 
-function closeAddBookForm(){
-    addModal.style.display = "none";
+function removeBook(){
+    //TODO: get reference to 
+    console.log("hi!");
+    // remove book from list
+
+    // re-draw library list
+    // displayBooks();
 }
 
-addBookBtn.addEventListener("click", openAddBookForm);
+function openAddBookForm() {
+  addModal.style.display = "block";
+}
+
+function closeAddBookForm() {
+  addModal.style.display = "none";
+  clearAddBookFormFields();
+}
+
+function clearAddBookFormFields() {
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+}
+
+// window.addEventListener("load", displayBooks);
+
+openModalBtn.addEventListener("click", openAddBookForm);
 
 closeModalBtn.addEventListener("click", closeAddBookForm);
+
+addBookBtn.addEventListener("click", addBookToLibrary);
